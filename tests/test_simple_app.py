05b2847c4177ef8b2b23b262e773799c46d150f0
Marcelo Trylesinski <marcelotryle@gmail.com>
2020-12-12 01:49:52 +0100

    ✅ Add tests
    Test simple and multiple exception based apps

--- tests/test_simple_app.py
+++ tests/test_simple_app.py
@@ -1,32 +1,17 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
+
 from fastapi_responses import custom_openapi
 
 app = FastAPI()
 
-
-def raise_another():
-    raise HTTPException(status_code=200, detail="Another function!")
-
-
-async def get_another_user():
-    raise HTTPException(status_code=304, detail="Yet another function!")
-
-
-def get_user(opa: str = Depends(get_another_user)):
-    raise HTTPException(status_code=201, detail="HAHA")
+app.openapi = custom_openapi(app)
 
 
 @app.get("/")
-def home(item: int, user: str = Depends(get_user)):
-    if item == 1:
-        raise HTTPException(
-            status_code=404, detail="I need a really long sentence so I can analyze..."
-        )
-    raise_another()
+def home():
+    return "Hello World!"
 
-
-app.openapi = custom_openapi(app)
 
 client = TestClient(app)
 
@@ -51,7 +36,8 @@ openapi_schema = {
 
 
 def test_simple_app():
-    openapi = client.get("/openapi.json/")
-    print(openapi.json())
-    assert openapi.json() == openapi_schema
-    assert False
+    res = client.get("/")
+    assert res.status_code == 200
+
+    res = client.get("/openapi.json/")
+    assert res.json() == openapi_schema
